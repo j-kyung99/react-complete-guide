7466cdd4159965f2c11b2ce56a89a0cff31d06c6
j-kyung99 <apfwnls14@naver.com>
2023-08-01 13:33:23 +0900

    #28 타입스크립트 기본 유형 및 배열, 객체 유형 작업

---
+++ project18/basics.ts
@@ -0,0 +1,30 @@
+let age: number;
+
+age = 12;
+
+let userName: string;
+
+userName = "Max";
+
+let isInstructor: boolean;
+
+isInstructor = true;
+
+let hobbies: string[];
+
+hobbies = ["Sports", "Cooking"];
+
+let person: {
+  name: string;
+  age: number;
+};
+
+person = {
+  name: "Max",
+  age: 32,
+};
+
+let people: {
+  name: string;
+  age: number;
+}[];
