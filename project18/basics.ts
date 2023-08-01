3061706d19c9385395a635c8a5a975e838207cc1
j-kyung99 <apfwnls14@naver.com>
2023-08-01 14:00:06 +0900

    #28 함수 및 함수 유형

--- project18/basics.ts
+++ project18/basics.ts
@@ -33,3 +33,13 @@ let people: Person[];
 let course: string | number = "React - The Complete Guide";
 
 course = 12341;
+
+// Functions & types
+
+function add(a: number, b: number) {
+  return a + b;
+}
+
+function pring(value: any) {
+  console.log(value);
+}
